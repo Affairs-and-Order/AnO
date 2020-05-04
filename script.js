ae88af684a3e2f9d17327587a24717b5b7fa3a59
CarsonBurke <48334001+CarsonBurke@users.noreply.github.com>
2020-05-03 18:19:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function menubardrop() {
+
+    document.getElementById("menubar").classList.toggle("menubarclick");
+
+    document.getElementById("menubardiv").classList.toggle("menubardivshow");
+
+    document.getElementById("menubardiv").style.transition = "margin 0.2s";
+
+    //    
+
+    document.getElementById("bar1").classList.toggle("barclick1");
+
+    document.getElementById("bar2").classList.toggle("barclick2");
+
+    document.getElementById("bar3").classList.toggle("barclick3");
+
+    //
+
+    document.getElementById("bar1").style.transition = "all 0.2s";
+
+    document.getElementById("bar2").style.transition = "all 0.2s";
+
+    document.getElementById("bar3").style.transition = "all 0.2s";
+}
