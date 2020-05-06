4bd3fb4edafff315328b4ce1f9c33cc9f7c3112c
Djcoldcrown <Coldcrown370@gmail.com>
2020-05-06 19:45:53 -0400

    Fixed casualties
    There was a bug where the casualties were given to the wrong nation(s) i fixed it

--- Ground attack.py
+++ Ground attack.py
@@ -2,7 +2,7 @@ import random
 
 Nat1 = 2000 # get from database for now lets call it 2000
 Nat2 = 1000 # get from database for now lets call it 1000
-#TODO: get nation namees from DB
+#TODO: get nation names from DB, get amount of soldiers from DB,add tanks to script,add infra damage
 random_roll_nat1 = random.randint(1,Nat1)
 random_roll_nat2 = random.randint(1,Nat1)
 
@@ -20,18 +20,18 @@ for roll in range(0,3):
     
     if nat1_roll > nat2_roll:
         nat1_roll_wins += 1
-        # subtract difference from the nations maximum if they rolled higher
-        Nat1 -= difference
-        # the twelve percent stored in new variable so it can be printed
+        # subtract difference from the nations maximum if they rolled lower
+        Nat2 -= difference
+        # gives a 12% casualty rate for the nation that rolled larger 
         twelve_percent_loss = int(Nat2*0.12)
-        Nat2 -= twelve_percent_loss
+        Nat1 -= twelve_percent_loss
     else:
         nat2_roll_wins += 1 
-        # subtract difference from the nations maximum if they rolled higher
-        Nat2 -= difference
+        # subtract difference from the nations maximum if they rolled lower
+        Nat1 -= difference
         # the twelve percent stored in new variable so it can be printed
         twelve_percent_loss = int(Nat1*0.12)
-        Nat1 -= twelve_percent_loss
+        Nat2 -= twelve_percent_loss
         
     print(f"This is the output after the battle: {difference}")
     print(f"This is the 12 perecent value from the battle: {twelve_percent_loss}")
