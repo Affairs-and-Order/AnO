f57a4acb401274c7cd1ad0097ae6fdd31b752cc1
CarsonBurke <carsonburke22@gmail.com>
2020-05-07 15:48:09 -0700

    update

--- script.js
+++ script.js
@@ -13,34 +13,8 @@ function menubardrop() {
 
     document.getElementById("bar3").classList.toggle("barclick3");
 }
-//loginshow
-function loginshow() {
 
-    document.getElementById("logindiv").classList.toggle("logindivshow");
 
-    document.getElementById("signupdiv").classList.remove("signupdivshow");
-
-}
-//loginclose
-function loginclose() {
-
-    document.getElementById("logindiv").classList.toggle("logindivshow");
-
-}
-//signupshow
-function signupshow() {
-
-    document.getElementById("signupdiv").classList.toggle("signupdivshow");
-
-    document.getElementById("logindiv").classList.remove("logindivshow");
-
-}
-//signupclose
-function signupclose() {
-
-    document.getElementById("signupdiv").classList.toggle("signupdivshow");
-
-}
 //
 $(document).ready(function() {
     console.log("jQuery ready");
