55e2f85660e9e7ca65904c3cb53c68f2afcd5aba
CarsonBurke <carsonburke22@gmail.com>
2020-05-06 23:54:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,20 +13,10 @@ function menubardrop() {
 
     document.getElementById("bar3").classList.toggle("barclick3");
 }
-
-var modalOpen = false;
-
 //loginshow
 function loginshow() {
 
-    // this is required so people cant open multiple modals at once
-
-    if (modalOpen == false) {
-        document.getElementById("logindiv").classList.toggle("logindivshow");
-        modalOpen = true;
-    } else {
-        return;
-    }
+    document.getElementById("logindiv").classList.toggle("logindivshow");
 
     document.getElementById("signupdiv").classList.remove("signupdivshow");
 
@@ -35,20 +25,12 @@ function loginshow() {
 function loginclose() {
 
     document.getElementById("logindiv").classList.toggle("logindivshow");
-    modalOpen = false;
 
 }
 //signupshow
 function signupshow() {
 
-     // this is required so people cant open multiple modals at once
-
-    if (modalOpen == false) {
-        document.getElementById("signupdiv").classList.toggle("signupdivshow");
-        modalOpen = true;
-    } else {
-        return;
-    }
+    document.getElementById("signupdiv").classList.toggle("signupdivshow");
 
     document.getElementById("logindiv").classList.remove("logindivshow");
 
@@ -57,7 +39,6 @@ function signupshow() {
 function signupclose() {
 
     document.getElementById("signupdiv").classList.toggle("signupdivshow");
-    modalOpen = false;
 
 }
 //
