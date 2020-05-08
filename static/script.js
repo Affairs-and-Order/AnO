2a9515805513ae595e94c93668c15272c3714afe
Carson Burke <carsonburke22@gmail.com>
2020-05-08 10:33:58 -0700

    update

---
+++ static/script.js
@@ -0,0 +1,22 @@
+//navbar events
+function menubardrop() {
+
+    document.getElementById("menubar").classList.toggle("menubarclick");
+
+    document.getElementById("menubardiv").classList.toggle("menubardivshow");
+
+    //    
+
+    document.getElementById("bar1").classList.toggle("barclick1");
+
+    document.getElementById("bar2").classList.toggle("barclick2");
+
+    document.getElementById("bar3").classList.toggle("barclick3");
+}
+
+
+//
+$(document).ready(function() {
+    console.log("jQuery ready");
+
+});
