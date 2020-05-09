cccf4441fc3e0ed4e0899611b2bf0f483e5ad8d7
delivey <deliveydev@gmail.com>
2020-05-09 12:18:21 +0300

    added checking for empty signup inputs

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template, session, redirect
 from flask_session import Session
 from tempfile import mkdtemp
 
@@ -21,9 +21,19 @@ def signup():
         print("todo")
     else:
         return render_template("signup.html")
+
 @app.route("/login")
 def login():
     if request.method == "POST":
-        print("todo")
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+        if not confirmation or not password or not email or not username:
+            return redirect("/") # TODO change to error
+        elif password != confirmation:
+            return redirect("/") # TODO change to error
+        else:
+            print("f")
     else:
         return render_template("login.html")
