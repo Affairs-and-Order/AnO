247e33ffc7baa2194f45fb513e3007a2f320450f
delivey <deliveydev@gmail.com>
2020-05-15 18:53:14 +0300

    celery is now working, i gotta find ways to time the script

--- app.py
+++ app.py
@@ -111,11 +111,20 @@ def signup():
 
 @celery.task()
 def populationGrowth():
-    connection = sqlite3.connect('affo/aao.db')
-    db = connection.cursor()
-    population = db.execute("SELECT population FROM stats").fetchall()
-    result = "dsd"
-    return result
+    conn = sqlite3.connect('affo/aao.db')
+    db = conn.cursor()
+
+    pop = db.execute("SELECT population, id FROM stats").fetchall()
+
+    for row in pop:
+        user_id = row[1]
+        curPop = row[0]
+        newPop = curPop + (int(curPop/10))
+        ple = db.execute("UPDATE stats SET population=(?) WHERE id=(?)", (newPop, user_id,))
+        conn.commit()
+
+    pop = db.execute("SELECT population FROM stats").fetchall()[0]
+    return pop
 
 @login_required
 @app.route("/country/id=<cId>")
--- testing.py
+++ testing.py
@@ -1,9 +1,21 @@
 import sqlite3
 
-conn = sqlite3.connect('affo/aao.db')
-db = conn.cursor()
+def populationGrowth():
+    
+    conn = sqlite3.connect('affo/aao.db')
+    db = conn.cursor()
 
-pop = db.execute("SELECT population, id FROM stats").fetchall()
+    pop = db.execute("SELECT population, id FROM stats").fetchall()
 
-for row in pop:
-    print(row[1], row[0])
+    for row in pop:
+        user_id = row[1]
+        curPop = row[0]
+        newPop = curPop + (int(curPop/10))
+        ple = db.execute("UPDATE stats SET population=(?) WHERE id=(?)", (newPop, user_id,))
+        conn.commit()
+
+    pop = db.execute("SELECT population FROM stats").fetchall()[0]
+    print(pop)
+
+populationGrowth()
+
