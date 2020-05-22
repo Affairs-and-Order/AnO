222a888d5e063536741f977badc03a9b8ffbb0c0
delivey <deliveydev@gmail.com>
2020-05-22 10:44:58 +0300

    added /market, fixed navbar, burger menu not rendering on certain pages

--- app.py
+++ app.py
@@ -167,18 +167,35 @@ def military():
 @login_required
 @app.route("/market")
 def market():
-    return render_template("market.html")
+    try: 
+        seshId = session["user_id"]
+        uId = True
+        return render_template("market.html", uId=uId, seshId=seshId)
+    except KeyError:
+        uId = False
+    return render_template("market.html", uId=uId)
 
 @login_required
 @app.route("/coalition")
 def coalition(): 
-    return render_template("coalition.html")
+    try: 
+        seshId = session["user_id"]
+        uId = True
+        return render_template("coalition.html", uId=uId, seshId=seshId)
+    except KeyError:
+        uId = False
+    return render_template("coalition.html", uId=uId)
 
 @login_required
 @app.route("/world")
 def world(): 
-    return render_template("world.html")
-
+    try: 
+        seshId = session["user_id"]
+        uId = True
+        return render_template("world.html", uId=uId, seshId=seshId)
+    except KeyError:
+        uId = False
+    return render_template("world.html", uId=uId)
 
 # available to run if double click the file
 if __name__ == "__main__":
