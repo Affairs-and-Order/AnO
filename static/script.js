633d69bf82d91e49c7242e29ddb191a58a096c94
Carson Burke <carsonburke22@gmail.com>
2020-05-25 17:42:19 -0700

    fully functional military page!

--- static/script.js
+++ static/script.js
@@ -17,6 +17,52 @@ function menubardrop() {
 
 //
 $(document).ready(function() {
+
+    document.getElementById("militaryleft").classList.add("militaryleftclick");
+    document.getElementById("land").classList.add("landclick");
+
     console.log("jQuery ready");
 
 });
+//
+function militaryleft() {
+
+    document.getElementById("militaryleft").classList.add("militaryleftclick");
+    document.getElementById("land").classList.add("landclick");
+
+    //
+
+    document.getElementById("militarycenter").classList.remove("militarycenterclick");
+    document.getElementById("air").classList.remove("airclick");
+
+    document.getElementById("militaryright").classList.remove("militaryrightclick");
+    document.getElementById("water").classList.remove("waterclick");
+}
+//
+function militarycenter() {
+
+    document.getElementById("militarycenter").classList.add("militarycenterclick");
+    document.getElementById("air").classList.add("airclick");
+
+    //
+
+    document.getElementById("militaryleft").classList.remove("militaryleftclick");
+    document.getElementById("land").classList.remove("landclick");
+
+    document.getElementById("militaryright").classList.remove("militaryrightclick");
+    document.getElementById("water").classList.remove("waterclick");
+}
+//
+function militaryright() {
+
+    document.getElementById("militaryright").classList.add("militaryrightclick");
+    document.getElementById("water").classList.add("waterclick");
+
+    //
+
+    document.getElementById("militaryleft").classList.remove("militaryleftclick");
+    document.getElementById("land").classList.remove("landclick");
+
+    document.getElementById("militarycenter").classList.remove("militarycenterclick");
+    document.getElementById("air").classList.remove("airclick");
+}
