96289e07cd6be7f065fec2557a64ff5fee15f761
delivey <deliveydev@gmail.com>
2020-06-09 08:51:30 +0300

    added script for auto db wiping, wiped db

---
+++ affo/wipe_and_create.py
@@ -0,0 +1,12 @@
+import sqlite3
+    
+conn = sqlite3.connect('aao.db')
+db = conn.cursor()
+
+tables = ["air", "coalitions", "colNames", "ground", "keys", "provinces",
+"requests", "special", "stats", "users", "water"]
+
+for i in tables:
+    db.execute(f"DROP TABLE {i}")
+    txt = open(f"raw/{i}.txt", "r")
+    db.execute(txt.read())
