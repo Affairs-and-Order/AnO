77900435625b6acd5e5a93d19062dd6b5a258114
Djcoldcrown <Coldcrown370@gmail.com>
2020-06-10 12:47:35 -0400

    Added bomber losses for both sides

--- attack_scripts/Air_attack.py
+++ attack_scripts/Air_attack.py
@@ -3,8 +3,7 @@ import random
 
 nat1_fighters = Nat1
 nat2_fighters = Nat2
-nat1_name = Danzig
-nat2_name = Konigsburg
+
 #TODO: get nation names from DB, get amount of fighters and bombers from DB,add infra damage,make aircraft consume resources
 #TODO: figure out how calculate bomber and fighter losses seperatly 
 
@@ -30,10 +29,13 @@ for roll in range(0, 3):
 		# subtract difference from the nations maximum if they rolled lower
 		Nat2 -= difference
 		#calulates bomber losses
-		Bombloss = random.randint (1, Nat2Bomb // 5) * 5
-		Nat2Bomb = Nat2Bomb - Bombloss / 5
-		displayloss = Bombloss / 5
-		print(f"Nation 2 lost {displayloss} Bombers")
+		Bombloss1 = random.randint (1, Nat1Bomb // 5) * 5
+		Nat1Bomb = Nat1Bomb - Bombloss / 5
+		displayloss1 = Bombloss1 / 5
+		Bombloss2 = random.randint (1, Nat2Bomb // 5) * 5
+		Nat2Bomb = Nat2Bomb - Bombloss2 / 5
+		displayloss2 = Bombloss2 / 5
+		print(f"Nation 2 lost {displayloss2} Bombers")
 		# gives a 6% casualty rate for the nation that rolled larger 
 		six_percent_loss = int(Nat2 * 0.06)
 		Nat1 -= six_percent_loss
@@ -48,13 +50,18 @@ for roll in range(0, 3):
 		# subtract difference from the nations maximum if they rolled lower
 		Nat1 -= difference
 		# the twelve percent stored in new variable so it can be printed
-		Bombloss = random.randint (1, Nat1Bomb // 5) * 5
+		Bombloss1 = random.randint (1, Nat1Bomb // 5) * 5
 		Nat1Bomb = Nat1Bomb - Bombloss / 5
-		displayloss = Bombloss / 5
+		displayloss1 = Bombloss1 / 5
+		Bombloss2 = random.randint (1, Nat2Bomb // 5) * 5
+		Nat2Bomb = Nat2Bomb - Bombloss2 / 5
+		displayloss2 = Bombloss2 / 5
+		print(f"Nation 2 lost {displayloss2} Bombers")
 		six_percent_loss = int(Nat1 * 0.06)
 		Nat2 -= six_percent_loss
-		#ends battle if all aircraft are destroyed
-		print(f"Nation 1 lost {displayloss} Bombers")
+		
+		print(f"Nation 1 lost {displayloss1} Bombers")
+		#endsbattle if all are destroyed
 		if  Nat1 and Nat1Bomb <= 0:
 			Nat1 = 0
 			Nat1Bomb = 0
@@ -68,5 +75,5 @@ if nat1_roll_wins > nat2_roll_wins:
 	print(f"{nat1_name} won the battle")
 else:
 	print(f"{Nat2_name} won the battle")
-	ground_attack("Danzig", "Konigsburg", 630, 380,4,10)
+ground_attack("Danzig", "Konigsburg", 630, 380,4,10)
 
