24fe9a6e78ca7e4e808fd18bd5a12a726b3dc187
delivey <deliveydev@gmail.com>
2020-06-25 13:57:37 +0300

    added basic postgresql config in wipe_and_create.py

---
+++ affo/wipe_and_create_postgres.py
@@ -0,0 +1,14 @@
+import psycopg2
+    
+conn = psycopg2.connect(database="postgres", user="postgres", password="ano", host="127.0.0.1", port="5432")
+db = conn.cursor()
+
+tables = ["air", "coalitions", "colNames", "ground", "provinces",
+"requests", "special", "stats", "users", "water", "offers", "resources"]
+
+for i in tables:
+    with open(f"raw/{i}.txt") as file:
+        db.execute(file.read())
+        print(f"Recreated table {i}")
+        conn.commit()
+
