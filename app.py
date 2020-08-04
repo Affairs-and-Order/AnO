540203fe51842804a2daac9a07fe0931a564642d
delivey <deliveydev@gmail.com>
2020-08-04 16:28:32 +0300

    fixed account creating on error

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def inject_user():
             inColit = db.execute("SELECT colId FROM coalitions WHERE userId=(?)", (session["user_id"], )).fetchone()[0]
             inCol = f"/coalition/{inColit}"
             return inCol
-        except:
+        except TypeError:
             inCol = error(404, "Page Not Found")
             return inCol
     def get_resource_amount():
@@ -214,21 +214,20 @@ def signup():
         
         if password != confirmation: # checks if password is = to confirmation password
             return error(400, "Passwords must match.")
-        for keys in allKeys: # lmao shitty way to do idk why i did this
-            if key == keys[0]:
+        for keys in allKeys: # lmao shitty way to do it idk why i did this im the epitomy of stupid
+            if key == keys[0]: # i should've just used a fucking select statement
                 hashed = generate_password_hash(password, method='pbkdf2:sha256', salt_length=32) # hashes the inputted password
                 db.execute("INSERT INTO users (username, email, hash, date) VALUES (?, ?, ?, ?)", (username, email, hashed, str(datetime.date.today()))) # creates a new user || added account creation date
                 user = db.execute("SELECT id FROM users WHERE username = (?)", (username,)).fetchone()
-                connection.commit()
                 session["user_id"] = user[0] # set's the user's "id" column to the sessions variable "user_id"
                 session["logged_in"] = True
 
-                db.execute("INSERT INTO stats (id, location) VALUES (SELECT id FROM users WHERE id = (?)), (?)", (session["user_id"], "Bosfront")) # change the default location                                                                          # "Bosfront" to something else
-                db.execute("INSERT INTO ground (id) SELECT id FROM users WHERE id = (?)", (session["user_id"],)) 
-                db.execute("INSERT INTO air (id) SELECT id FROM users WHERE id = (?)", (session["user_id"],))
-                db.execute("INSERT INTO water (id) SELECT id FROM users WHERE id = (?)", (session["user_id"],))
-                db.execute("INSERT INTO special (id) SELECT id FROM users WHERE id = (?)", (session["user_id"],))
-                db.execute("INSERT INTO resources (id) SELECT id FROM users WHERE id = (?)", (session["user_id"],))
+                db.execute("INSERT INTO stats (id, location) VALUES (?, ?)", ((session["user_id"]), ("Bosfront"))) # change the default location                                                                          # "Bosfront" to something else
+                db.execute("INSERT INTO ground (id) VALUES (?)", (session["user_id"],)) 
+                db.execute("INSERT INTO air (id) VALUES (?)", (session["user_id"],))
+                db.execute("INSERT INTO water (id) VALUES (?)", (session["user_id"],))
+                db.execute("INSERT INTO special (id) VALUES (?)", (session["user_id"],))
+                db.execute("INSERT INTO resources (id) VALUES (?)", (session["user_id"],))
 
                 db.execute("DELETE FROM keys WHERE key=(?)", (key,)) # deletes the used key
                 connection.commit()
@@ -866,6 +865,8 @@ def countries(): # TODO: fix shit ton of repeated code in function
         influences = []
 
         for i in users:
+
+            print(f"IIIIIIIIII: {i[0]}")
 
             date = db.execute("SELECT date FROM users WHERE id=(?)", (str(i[0]),)).fetchone()[0]
             dates.append(date)
