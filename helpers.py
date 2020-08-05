1e71de9cda3c243365ccf403d5035b98eaa3a483
delivey <deliveydev@gmail.com>
2020-08-05 09:45:41 +0300

    fixed countries, etc not working

--- helpers.py
+++ helpers.py
@@ -25,6 +25,7 @@ def get_influence(country_id):
     cId = country_id
     # ground
 
+    """
     tanks = db.execute("SELECT tanks FROM ground WHERE id=(?)", (cId,)).fetchone()[0]
     soldiers = db.execute("SELECT soldiers FROM ground WHERE id=(?)", (cId,)).fetchone()[0]
     artillery = db.execute("SELECT artillery FROM ground WHERE id=(?)", (cId,)).fetchone()[0]
@@ -47,11 +48,11 @@ def get_influence(country_id):
     military = tanks + soldiers + artillery + \
     flying_fortresses + fighter_jets + apaches +\
     destroyers + cruisers + submarines + \
-    spies + icbms + nukes
+    spies + icbms + nukes"""
     
-    gold = db.execute("SELECT gold FROM stats WHERE id=(?)", (cId,)).fetchone()[0]
+    # gold = db.execute("SELECT gold FROM stats WHERE id=(?)", (cId,)).fetchone()[0]
 
-    influence = military + gold
+    influence = 1000 # gold
 
     return influence
 
@@ -69,44 +70,20 @@ def get_coalition_influence(coalition_id):
 
     return total_influence
 
-def get_resource_amount():
-    conn = sqlite3.connect('affo/aao.db') # connects to db
-    db = conn.cursor()
-    session_id = session["user_id"]
-
-    money = db.execute("SELECT gold FROM stats WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-
-    rations = 0 # db.execute("SELECT rations FROM resources WHERE userId=(?)", (session_id,)).fetchone()[0]
-    oil = db.execute("SELECT oil FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-    coal = db.execute("SELECT coal FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-    uranium = db.execute("SELECT uranium FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-    bauxite = db.execute("SELECT bauxite FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-    iron = db.execute("SELECT iron FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-    lead = db.execute("SELECT lead FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-    copper = db.execute("SELECT copper FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-
-    components = db.execute("SELECT components FROM resources WHERE id=(?)", (session_id,)).fetchone()[0]
-    steel = db.execute("SELECT steel FROM resources WHERE id=(?)", (session_id,)).fetchone()[0]
-    consumer_goods = db.execute("SELECT consumer_goods FROM resources WHERE id=(?)", (session_id,)).fetchone()[0] # DONE
-
-    copper_plates = db.execute("SELECT copper_plates FROM resources WHERE id=(?)", (session_id,)).fetchone()[0]
-    aluminium = db.execute("SELECT aluminium FROM resources WHERE id=(?)", (session_id,)).fetchone()[0]
-    gasoline = db.execute("SELECT gasoline FROM resources WHERE id=(?)", (session_id,)).fetchone()[0]
-    ammunition = db.execute("SELECT ammunition FROM resources WHERE id=(?)", (session_id,)).fetchone()[0]
-    
-    lst = [money, rations, oil, coal, uranium, bauxite, iron, lead, copper, components, steel, consumer_goods, copper_plates, aluminium, gasoline, ammunition]
-    return lst
-
 def try_col():
     conn = sqlite3.connect('affo/aao.db') # connects to db
     db = conn.cursor()
     try:
-        inColit = db.execute("SELECT colId FROM coalitions WHERE userId=(?)", (session["user_id"], )).fetchone()[0]
-        inCol = f"/coalition/{inColit}"
-        return inCol
-    except RecursionError:
-        inCol = error(404, "Page Not Found")
-        return inCol
-    except TypeError:
+        try:
+            inColit = db.execute("SELECT colId FROM coalitions WHERE userId=(?)", (session["user_id"], )).fetchone()[0]
+            inCol = f"/coalition/{inColit}"
+            return inCol
+        except RecursionError:
+            inCol = error(404, "Page Not Found")
+            return inCol
+        except TypeError:
+            inCol = error(404, "Page Not Found")
+            return inCol
+    except KeyError:
         inCol = error(404, "Page Not Found")
         return inCol
