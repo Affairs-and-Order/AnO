b9c14fbf2f06f95ab46916d55cf3448a62072e70
delivey <deliveydev@gmail.com>
2020-08-11 11:32:38 +0300

    completed function for adding and removing production amounts

--- testing.py
+++ testing.py
@@ -14,22 +14,15 @@ infra = {
     'solar_fields_minus': {'oil': 0}
 }
 
-"""
-oil_burners_plus = tuple(infra['oil_burners_plus'])
-
-oil_plus_resource = oil_burners_plus[0]
-oil_plus_amount = oil_burners_plus[1]
-
-
-oil_burners_minus = tuple(infra['oil_burners_minus'])
-"""
-
 def generate_revenue():
 
     conn = sqlite3.connect('affo/aao.db') # connects to db
     db = conn.cursor()
 
     columns = ['oil_burners', 'hydro_dams', 'nuclear_reactors', 'solar_fields']
+
+    infra_ids = db.execute("SELECT id FROM proInfra").fetchall()
+
     for unit in columns:
         plus_data = next(iter(infra[f'{unit}_plus'].items()))
 
@@ -41,33 +34,33 @@ def generate_revenue():
         minus_resource = minus_data[0]
         minus_amount = minus_data[1]
 
+        """
         print(f"Unit: {unit}")
         print(f"Add {plus_amount} to {plus_resource}")
         print(f"Remove {minus_amount} of {minus_resource}")
-        print("\n")
+        print(f"\n")
+        """
+    
+        for province_id in infra_ids:
 
-    infra_ids = db.execute("SELECT id FROM proInfra").fetchall()
+            province_id = province_id[0]
 
-    """
-    for province_id in infra_ids:
+            user_id = db.execute("SELECT userId FROM provinces WHERE id=(?)", (province_id,)).fetchone()[0]
 
-        province_id = province_id[0]
+            ### REMOVING
+            current_minus_resource = db.execute(f"SELECT {minus_resource} FROM resources WHERE id=(?)", (user_id,)).fetchone()[0]
+            if current_minus_resource < minus_amount:
+                continue
+            else:
+                new_minus_resource_amount = current_minus_resource - minus_amount
+                db.execute(f"UPDATE resources SET {minus_resource}=(?) WHERE id=(?)", (new_minus_resource_amount, user_id))
 
-        user_id = db.execute("SELECT userId FROM provinces WHERE id=(?)", (province_id,)).fetchone()[0]
-
-        current_oil = db.execute("SELECT oil FROM resources WHERE id=(?)", (user_id,)).fetchone()[0]
-        if current_oil < 4:
-            continue
-        else:
-            new_oil = current_oil - 4
-            db.execute("UPDATE resources SET oil=(?) WHERE id=(?)", (new_oil, user_id))
-            # right now oil burners make 12 uranium while burning 4 oil
-            current_resource_number = db.execute("SELECT uranium FROM resources WHERE id=(?)", (user_id,)).fetchone()[0]
-            new_resource_number = current_resource_number + 12 # 12 is how many uranium it generates
-            db.execute("UPDATE resources SET uranium=(?) WHERE id=(?)", (new_resource_number, user_id))
+                ### ADDING
+                current_plus_resource = db.execute(f"SELECT {plus_resource} FROM resources WHERE id=(?)", (user_id,)).fetchone()[0]
+                new_resource_number = current_plus_resource + plus_amount # 12 is how many uranium it generates
+                db.execute(f"UPDATE resources SET {plus_resource}=(?) WHERE id=(?)", (new_resource_number, user_id))
 
     conn.commit()
     conn.close()
-    """
 
 generate_revenue()
