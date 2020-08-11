f34f23a4a39508c1ac70c22128ed56023cbf92f2
delivey <deliveydev@gmail.com>
2020-08-11 11:10:31 +0300

    added system for figuring out each unit's production

--- testing.py
+++ testing.py
@@ -1,16 +1,58 @@
 import sqlite3
 
+infra = {
+    'oil_burners_plus': {'uranium': 12},
+    'oil_burners_minus': {'oil': 4},
+
+    'hydro_dams_plus': {'uranium': 8},
+    'hydro_dams_minus': {'oil': 2},
+
+    'nuclear_reactors_plus': {'uranium': 24},
+    'nuclear_reactors_minus': {'iron': 8},
+
+    'solar_fields_plus': {'iron': 4},
+    'solar_fields_minus': {'oil': 0}
+}
+
+"""
+oil_burners_plus = tuple(infra['oil_burners_plus'])
+
+oil_plus_resource = oil_burners_plus[0]
+oil_plus_amount = oil_burners_plus[1]
+
+
+oil_burners_minus = tuple(infra['oil_burners_minus'])
+"""
+
 def generate_revenue():
 
     conn = sqlite3.connect('affo/aao.db') # connects to db
     db = conn.cursor()
 
-    # columns = ['oil_burners', 'hydro_dams', 'nuclear_reactors', 'solar_fields']
-    oil_burners = db.execute("SELECT id FROM proInfra").fetchall()
-    for province_id in oil_burners:
+    columns = ['oil_burners', 'hydro_dams', 'nuclear_reactors', 'solar_fields']
+    for unit in columns:
+        plus_data = next(iter(infra[f'{unit}_plus'].items()))
+
+        plus_resource = plus_data[0]
+        plus_amount = plus_data[1]
+
+        minus_data = next(iter(infra[f'{unit}_minus'].items()))
+
+        minus_resource = minus_data[0]
+        minus_amount = minus_data[1]
+
+        print(f"Unit: {unit}")
+        print(f"Add {plus_amount} to {plus_resource}")
+        print(f"Remove {minus_amount} of {minus_resource}")
+        print("\n")
+
+    infra_ids = db.execute("SELECT id FROM proInfra").fetchall()
+
+    """
+    for province_id in infra_ids:
 
         province_id = province_id[0]
-        
+
         user_id = db.execute("SELECT userId FROM provinces WHERE id=(?)", (province_id,)).fetchone()[0]
 
         current_oil = db.execute("SELECT oil FROM resources WHERE id=(?)", (user_id,)).fetchone()[0]
@@ -26,5 +68,6 @@ def generate_revenue():
 
     conn.commit()
     conn.close()
+    """
 
 generate_revenue()
