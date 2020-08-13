61ea68f371ff29d5ec856b4569c8ef79c2ae92b3
Steven Kim <97stevenkim@gmail.com>
2020-08-12 23:48:37 -0400

    warResult.html, warGet.html, cleaning of initial war attack route

--- app.py
+++ app.py
@@ -400,7 +400,7 @@ def wars():
 
 
 @login_required
-@app.route("/wars", methods=["GET", "POST"])
+@app.route("/wars/<attackingNation>/defendingNation", methods=["GET", "POST"])
 def wars():
 
     connection = sqlite3.connect('affo/aao.db')
@@ -408,67 +408,10 @@ def wars():
     cId = session["user_id"]
 
     if request.method == "GET":  # maybe optimise this later with css anchors
-        # obtain all ground unit numbers from sql table
-        tanks = db.execute(
-            "SELECT tanks FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        soldiers = db.execute(
-            "SELECT soldiers FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        artillery = db.execute(
-            "SELECT artillery FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        # obtain all air unit numbers from sql table
-        flying_fortresses = db.execute(
-            "SELECT flying_fortresses FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        fighter_jets = db.execute(
-            "SELECT fighter_jets FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        apaches = db.execute(
-            "SELECT apaches FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        # obtain all navy unit numbers from sql table
-        destroyers = db.execute(
-            "SELECT destroyers FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        cruisers = db.execute(
-            "SELECT cruisers FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        submarines = db.execute(
-            "SELECT submarines FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        # obtain all special unit numbers from sql table
-        spies = db.execute(
-            "SELECT spies FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        icbms = db.execute(
-            "SELECT ICBMs FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        nukes = db.execute(
-            "SELECT nukes FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-
-        # obtain the user's country from sql table
-        yourCountry = db.execute(
-            "SELECT username FROM users WHERE id=(?)", (cId,)).fetchone()[0]
-
-        # this creates an array called attacking which stores tuples in the format [(defendingCountryName1, defendingCountryUsername1), (defendingCountryName2, defendingCountryUsername2), ...].
-        try:
-            # selecting all current defenders of cId
-            attackingWars = db.execute(
-                "SELECT defender FROM wars WHERE attacker=(?) ORDER BY defender", (cId,)).fetchall()
-            # selecting all usernames of current defenders of cId
-            attackingNames = db.execute(
-                "SELECT username FROM users WHERE id=(SELECT defender FROM wars WHERE attacker=(?) ORDER BY defender)", (cId,)).fetchall()
-            # generates list of tuples. The first element of each tuple is the country being attacked, the second element is the username of the countries being attacked.
-            attacking = zip(attackingWars, attackingNames)
-        except TypeError:
-            attacking = 0
-
-        # gets a defending tuple
-        try:
-            defendingWars = db.execute(
-                "SELECT attacker FROM wars WHERE defender=(?) ORDER BY defender", (cId,)).fetchall()
-            defendingNames = db.execute(
-                "SELECT username FROM users WHERE id=(SELECT attacker FROM wars WHERE defender=(?) ORDER BY defender)", (cId,)).fetchall()
-            defending = zip(defendingWars, defendingNames)
-        except TypeError:
-            defending = 0
-
-        # gets a warsCount value
-        warsCount = db.execute(
-            "SELECT COUNT(attacker) FROM wars WHERE defender=(?) OR attacker=(?)", (cId, cId)).fetchone()[0]
-
-        # returns ALL the VALUES to wars.html
+        # if the method is get THE USER IS PROBABLY HACKING
+        return render_template("badresult.html")
+    if request.method == "POST"
+        # returns ALL the VALUES to warResult.html
         return render_template("wars.html", tanks=tanks, soldiers=soldiers, artillery=artillery,
                                flying_fortresses=flying_fortresses, fighter_jets=fighter_jets, apaches=apaches,
                                destroyers=destroyers, cruisers=cruisers, submarines=submarines,
