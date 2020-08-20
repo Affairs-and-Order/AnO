c3ee8252bcd91a60f8a9ce667b63d9bec1782e73
Steven Kim <97stevenkim@gmail.com>
2020-08-20 08:34:58 -0400

    added testroutes.py

--- WarScript.py
+++ WarScript.py
@@ -1,65 +1,6 @@
-# every single line in this file here is useless for now
+from app import app
 
 
-import random
-import sqlite3
-import _pickle as pickle
-import json
-import random
-import sqlite3
-
-path = "affo/aao.db"
-conn = sqlite3.connect(path)
-db = conn.cursor() # syntax sqlite3 to convert to usable python interface
-
-# runs every 2 hours  (12pm, 2pm, 4pm, etc) check if any countries at war have not fought in 2 hours. if they have not fought in 2 hours, update the time they last fought and fight. Else just nothing happens... till the next 2 hours anyway. The user interface must also not allow clicking "attack" on the same country if they are already at war.
-# THIS METHOD IS OBSOLETE USE SUPPLIES INSTEAD OF AUTO ATTACK EVERY 2 HOURS
-
-def WarsHappenNow():
-    # each table entry was put there either immediately because of a click, or because of a 2 hour later war result update
-    # I expect each entry in the WarTable to have: attackingCountry, defendingCountry,
-
-    # Find all entries where the ACTIVE value is TRUE, meaning current wars.
-    # all the entries where ACTIVE is False are just for history.
-    allLatestWarEntries = db.execute(
-        "SELECT * FROM wartable WHERE ACTIVE=TRUE").fetchall()
-
-    # go through one war at a time.
-    for warEntry in allLatestWarEntries:
-
-            print('you are already fighting this country!')
-            db.execute(
-                "read from database(countryIDattacker): units, morale, tech_score, effectiveness").fetchall()
-            # x = lambda n, m, v, t, e : (n+v) * ((t*e)/10 + m)
-            # use the multiplication for now, later the different unit weights can be factored in. etc: fight ground vs ground, sea vs sea, air vs air, giving bonuses if a certain fight was completely dominated.
-            #country1result = units*morale * \
-            #    tech_score*effectiveness*Math.random(1)
-
-            #sql.execute(new_units, new_morale,
-            #            new_techscore, new_effectiveness)
-
-
-
-        #db.execute upload TABLE: WAR_TABLE
-
-def declareWarButtonPressed(attackerID, defenderID):
-        attackerProvinces = db.execute("SELECT provinces FROM war WHERE ").fetchall()
-        defendingProvinces = db.execute("SELECT provinces FROM war WHERE ").fetchall() #read from database(countryIDattacker): provinces
-        # if they have the same number of provinces then allow the war to happen
-        if attackerProvinces == defendingProvinces:
-            # war starts
-            print('war allowed put war entry in war table.')
-            db.execute("NEW ENTRY TO WARTABLE ")
-        # find if the number of provinces for each country is the same in that war
-        else:
-            print('this country does not have the same number of provinces as you!')
-
-def helpAlly(helperID, attackerID, troops):
-    db.execute("MODIFY TABLE WAR (troops) WHERE id=attackerID")
-    db.execute("MODIFY TABLE WAR (troops) WHERE id=helperID")
-
-
-# nation1score = multiplies all these inputs together. 
-# nation2score = multiplies all the 2nd inputs together.
-#if nation1score > nation2score:
-    # some kind of destruction, this will be on a per round basis just like in pnw.
+@app.route('/testroute2', methods=['GET'])
+def testfunc2():
+    return 'it works!2'
--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, request, render_template, session, redirect
+from flask import Flask, request, render_template, session, redirect, abort
 from flask_session import Session
 from tempfile import mkdtemp
 import sqlite3
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from helpers import login_required, error
-# import WarScript # entirely useless file ignore this for now
 import datetime
 import _pickle as pickle
 import random
@@ -316,114 +315,6 @@ person = {"name": "galaxy"}
 person["message"] = "Thanks guys :D, you are all so amazing." # easter egg probably or it has something to do with mail xD || aw thats nice
 
 
-#########################################################################
-
-
-@login_required
-@app.route("/wars", methods=["GET", "POST"])
-def wars():
-
-    connection = sqlite3.connect('affo/aao.db')
-    db = connection.cursor()
-    cId = session["user_id"]
-
-    if request.method == "GET":  # maybe optimise this later with css anchors
-        # obtain all ground unit numbers from sql table
-        tanks = db.execute(
-            "SELECT tanks FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        soldiers = db.execute(
-            "SELECT soldiers FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        artillery = db.execute(
-            "SELECT artillery FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        # obtain all air unit numbers from sql table
-        flying_fortresses = db.execute(
-            "SELECT flying_fortresses FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        fighter_jets = db.execute(
-            "SELECT fighter_jets FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        apaches = db.execute(
-            "SELECT apaches FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        # obtain all navy unit numbers from sql table
-        destroyers = db.execute(
-            "SELECT destroyers FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        cruisers = db.execute(
-            "SELECT cruisers FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        submarines = db.execute(
-            "SELECT submarines FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        # obtain all special unit numbers from sql table
-        spies = db.execute(
-            "SELECT spies FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        icbms = db.execute(
-            "SELECT ICBMs FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-        nukes = db.execute(
-            "SELECT nukes FROM military WHERE id=(?)", (cId,)).fetchone()[0]
-
-        # obtain the user's country from sql table
-        yourCountry = db.execute(
-            "SELECT username FROM users WHERE id=(?)", (cId,)).fetchone()[0]
-
-        # this creates an array called attacking which stores tuples in the format [(defendingCountryName1, defendingCountryUsername1), (defendingCountryName2, defendingCountryUsername2), ...].
-        try:
-            # selecting all current defenders of cId
-            attackingWars = db.execute(
-                "SELECT defender FROM wars WHERE attacker=(?) ORDER BY defender", (cId,)).fetchall()
-            # selecting all usernames of current defenders of cId
-            attackingNames = db.execute(
-                "SELECT username FROM users WHERE id=(SELECT defender FROM wars WHERE attacker=(?) ORDER BY defender)", (cId,)).fetchall()
-            # generates list of tuples. The first element of each tuple is the country being attacked, the second element is the username of the countries being attacked.
-            attacking = zip(attackingWars, attackingNames)
-        except TypeError:
-            attacking = 0
-        
-        # gets a defending tuple
-        try:
-            defendingWars = db.execute(
-                "SELECT attacker FROM wars WHERE defender=(?) ORDER BY defender", (cId,)).fetchall()
-            defendingNames = db.execute(
-                "SELECT username FROM users WHERE id=(SELECT attacker FROM wars WHERE defender=(?) ORDER BY defender)", (cId,)).fetchall()
-            defending = zip(defendingWars, defendingNames)
-        except TypeError:
-            defending = 0
-
-        # gets a warsCount value
-        warsCount = db.execute(
-            "SELECT COUNT(attacker) FROM wars WHERE defender=(?) OR attacker=(?)", (cId, cId)).fetchone()[0]
-
-        # returns ALL the VALUES to wars.html
-        return render_template("wars.html", tanks=tanks, soldiers=soldiers, artillery=artillery,
-                               flying_fortresses=flying_fortresses, fighter_jets=fighter_jets, apaches=apaches,
-                               destroyers=destroyers, cruisers=cruisers, submarines=submarines,
-                               spies=spies, icbms=icbms, nukes=nukes, cId=cId, yourCountry=yourCountry,
-                               warsCount=warsCount, defending=defending, attacking=attacking)
-
-# the flask route that activates when you click attack on a nation in your wars page.
-# check if you have enough supplies.
-
-
-@login_required
-@app.route("/wars/<attackingNation>/defendingNation", methods=["GET", "POST"])
-def wars_route(attackingNation, defendingNation):
-
-    connection = sqlite3.connect('affo/aao.db')
-    db = connection.cursor()
-    cId = session["user_id"]
-
-    if request.method == "GET":
-        # if the method is get THE USER IS PROBABLY HACKING (or at least they typed in the URL directly which should never happen, maybe by a bookmark)
-        return render_template("badresult.html")
-    if request.method == "POST": 
-        # returns ALL the VALUES to warResult.html
-        return render_template("wars.html") 
-        """tanks=tanks, soldiers=soldiers, artillery=artillery,
-                                flying_fortresses=flying_fortresses, fighter_jets=fighter_jets, apaches=apaches,
-                                destroyers=destroyers, cruisers=cruisers, submarines=submarines,
-                                spies=spies, icbms=icbms, nukes=nukes, cId=cId, yourCountry=yourCountry,
-                                warsCount=warsCount, defending=defending, attacking=attacking"""
-
-# if everything went through, remove the cost of supplies from the amount of supplies the country has.
-
-
-############################################################################
-############################################################################
 
 
 @login_required
@@ -1404,6 +1295,9 @@ def my_coalition():
     else:
         return redirect(f"/coalition/{coalition}")
 
+# import declared routes
+from testroutes import testfunc
+from WarScript import testfunc2
 # available to run if double click the file
 if __name__ == "__main__":
     app.run(debug=True) # Runs the app with debug mode on
