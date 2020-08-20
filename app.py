f35e76461dd0fb3159bdc9b3cf95801870162e7e
Steven Kim <97stevenkim@gmail.com>
2020-08-20 08:54:53 -0400

    fixed login import and added signup separation

--- app.py
+++ app.py
@@ -151,53 +151,6 @@ def inject_user():
 def index():
     return render_template("index.html") # renders index.html when "/" is accesed
 
-
-@app.route("/signup", methods=["GET", "POST"])
-def signup():
-    if request.method == "POST":
-        
-        # connects the db to the signup function
-        connection = sqlite3.connect('affo/aao.db') # connects to db
-        db = connection.cursor()
-
-        username = request.form.get("username") # gets corresponding form inputs
-        email = request.form.get("email")
-        password = request.form.get("password")
-        confirmation = request.form.get("confirmation")
-
-        key = request.form.get("key")
-
-        allKeys = db.execute("SELECT key FROM keys").fetchall()
-        
-        if password != confirmation: # checks if password is = to confirmation password
-            return error(400, "Passwords must match.")
-        for keys in allKeys: # lmao shitty way to do it idk why i did this im the epitomy of stupid
-            if key == keys[0]: # i should've just used a fucking select statement
-                hashed = generate_password_hash(password, method='pbkdf2:sha256', salt_length=32) # hashes the inputted password
-                db.execute("INSERT INTO users (username, email, hash, date) VALUES (?, ?, ?, ?)", (username, email, hashed, str(datetime.date.today()))) # creates a new user || added account creation date
-                user = db.execute("SELECT id FROM users WHERE username = (?)", (username,)).fetchone()
-                session["user_id"] = user[0] # set's the user's "id" column to the sessions variable "user_id"
-                session["logged_in"] = True
-
-                db.execute("INSERT INTO stats (id, location) VALUES (?, ?)", ((session["user_id"]), ("Bosfront"))) #TODO  change the default location 
-                
-                db.execute("INSERT INTO military (id) VALUES (?)", (session["user_id"],))
-                db.execute("INSERT INTO resources (id) VALUES (?)", (session["user_id"],))
-
-                """
-                db.execute("INSERT INTO ground (id) VALUES (?)", (session["user_id"],)) 
-                db.execute("INSERT INTO air (id) VALUES (?)", (session["user_id"],))
-                db.execute("INSERT INTO water (id) VALUES (?)", (session["user_id"],))
-                db.execute("INSERT INTO special (id) VALUES (?)", (session["user_id"],))
-                """
-
-                db.execute("DELETE FROM keys WHERE key=(?)", (key,)) # deletes the used key
-                connection.commit()
-                connection.close()
-                return redirect("/")
-    else:
-        return render_template("signup.html")
-
 @login_required
 @app.route("/country/id=<cId>")
 def country(cId):
@@ -1135,6 +1088,8 @@ def my_coalition():
 from testroutes import testfunc
 from WarScript import wars, wars_route
 from market import market, buy_market_offer
+from login import login
+from signup import signup
 # available to run if double click the file
 if __name__ == "__main__":
     app.run(debug=True) # Runs the app with debug mode on
