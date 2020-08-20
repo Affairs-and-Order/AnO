79e0ea7a5721c4212c6213a3abc7a0976d83cbbf
Steven Kim <97stevenkim@gmail.com>
2020-08-20 08:51:43 -0400

    login separation file

--- app.py
+++ app.py
@@ -151,42 +151,6 @@ def inject_user():
 def index():
     return render_template("index.html") # renders index.html when "/" is accesed
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-
-    if request.method == "POST":
-
-        connection = sqlite3.connect('affo/aao.db') # connects to db
-        db = connection.cursor() # creates the cursor for db connection
-
-        password = request.form.get("password") # gets the password input from the form
-        username = request.form.get("username") # gets the username input from the forms
-
-        if not username or not password: # checks if inputs are blank
-            return error(400, "No Password or Username")
-
-        user = db.execute("SELECT * FROM users WHERE username = (?)", (username,)).fetchone() # selects data about user, from users
-        connection.commit()
-
-        if user is not None and check_password_hash(user[4], password): # checks if user exists and if the password is correct
-            session["user_id"] = user[0] # sets session's user_id to current user's id
-            try:
-                coalition = db.execute("SELECT colId FROM coalitions WHERE userId=(?)", (session["user_id"], )).fetchone()[0]
-            except TypeError:
-                coalition = error(404, "Page Not Found")
-
-            # print(f"coalition = {coalition}")
-            
-            session["coalition"] = coalition
-            print('User has succesfully logged in.')
-            connection.commit()
-            connection.close()
-            return redirect("/") # redirects user to homepage
-
-        return error(403, "Wrong password")
-
-    else:
-        return render_template("login.html") # renders login.html when "/login" is acessed via get
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
---
+++ login.py
@@ -0,0 +1,61 @@
+from flask import Flask, request, render_template, session, redirect, flash
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import generate_password_hash, check_password_hash
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+import datetime
+import _pickle as pickle
+import random
+from celery import Celery
+from helpers import login_required, error
+import sqlite3
+# from celery.schedules import crontab # arent currently using but will be later on
+from helpers import get_influence, get_coalition_influence
+# Game.ping() # temporarily removed this line because it might make celery not work
+from app import app
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+
+        connection = sqlite3.connect('affo/aao.db')  # connects to db
+        db = connection.cursor()  # creates the cursor for db connection
+
+        # gets the password input from the form
+        password = request.form.get("password")
+        # gets the username input from the forms
+        username = request.form.get("username")
+
+        if not username or not password:  # checks if inputs are blank
+            return error(400, "No Password or Username")
+
+        # selects data about user, from users
+        user = db.execute(
+            "SELECT * FROM users WHERE username = (?)", (username,)).fetchone()
+        connection.commit()
+
+        # checks if user exists and if the password is correct
+        if user is not None and check_password_hash(user[4], password):
+            # sets session's user_id to current user's id
+            session["user_id"] = user[0]
+            try:
+                coalition = db.execute(
+                    "SELECT colId FROM coalitions WHERE userId=(?)", (session["user_id"], )).fetchone()[0]
+            except TypeError:
+                coalition = error(404, "Page Not Found")
+
+            # print(f"coalition = {coalition}")
+
+            session["coalition"] = coalition
+            print('User has succesfully logged in.')
+            connection.commit()
+            connection.close()
+            return redirect("/")  # redirects user to homepage
+
+        return error(403, "Wrong password")
+
+    else:
+        # renders login.html when "/login" is acessed via get
+        return render_template("login.html")
