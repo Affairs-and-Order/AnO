6c843db02b534657f5f0290946c6895190e087e4
delivey <deliveydev@gmail.com>
2020-08-30 13:34:25 +0300

    added some more units to province infra gen

--- testing.py
+++ testing.py
@@ -1,11 +1,123 @@
 import sqlite3
 
-conn = sqlite3.connect('affo/aao.db') # connects to db
-db = conn.cursor()
+def generate_province_revenue(): # Runs each turn
 
-cId = 1
+    infra = {
+    ## ELECTRICITY
+    'oil_burners_plus': {'energy': 3},
+    'oil_burners_money': 60000,
+    'oil_burners_pollution': 25,
 
-my_offers = db.execute("SELECT resource, price, amount, type, offer_id FROM offers WHERE user_id=(?) ORDER BY offer_id ASC", (cId,)).fetchall()
+    'hydro_dams_plus': {'energy': 6},
+    'hydro_dams_money': 250000,
 
-print(my_offers)
+    'nuclear_reactors_plus': {'energy': 12}, #  Generates 12 energy to the province
+    'nuclear_reactors_money': 1200000, # Costs $1.2 million to operate nuclear reactor for each turn
+
+    'solar_fields_plus': {'energy': 3},
+    'solar_fields_money': 150000, # Costs $1.5 million
+
+    ## RETAIL (requires city slots)
+    'gas_stations_plus': {'consumer_goods': 4},
+    'gas_stations_money': 20000, # Costs $20k
+
+    'general_stores_plus': {'consumer_goods': 9},
+    'general_stores_money': 35000, # Costs $35k
+
+    'farmers_markets_plus': {'consumer_goods': 15}, # Generates 15 consumer goods
+    'farmers_markets_money': 110000, # Costs $110k
+
+    'malls_plus': {'consumer_goods': 22},
+    'malls_money': 300000, # Costs $300k
+
+    'banks_plus': {'consumer_goods': 30},
+    'banks_money': 800000, # Costs $800k
+
+    'city_parks': {'happiness': 3},
+    'city_parks_money': 20000, # Costs $20k
+
+    'hospitals': {'happiness': 8},
+    'hospitals_money': 60000,
+
+    'libraries': {'happiness': 5},
+    'libraries_money': 90000,
+    # Add productivity too
+
+    'universities': {'productivity': 12},
+    'universities_money': 150000,
+
+    'monorails': {'productivity': 15},
+    'monorails_money': 210000
+
+    }
+
+
+
+    conn = sqlite3.connect('affo/aao.db') # connects to db
+    db = conn.cursor()
+
+    columns = ['oil_burners', 'hydro_dams', 'nuclear_reactors', 'solar_fields']
+
+    infra_ids = db.execute("SELECT id FROM proInfra").fetchall()
+
+    for unit in columns:
+        
+        plus_data = next(iter(infra[f'{unit}_plus'].items()))
+
+        plus_resource = plus_data[0]
+        plus_amount = plus_data[1]
+
+        operating_costs = int(infra[f'{unit}_money'])
+
+        try:
+            pollution_amount = int(infra[f'{unit}_pollution'])
+        except KeyError:
+            pollution_amount = None
+        
+        """
+        print(f"Unit: {unit}")
+        print(f"Add {plus_amount} to {plus_resource}")
+        print(f"Remove ${operating_costs} as operating costs")
+        print(f"\n")
+        """
+        
+        for province_id in infra_ids:
+
+            province_id = province_id[0]
+
+            user_id = db.execute("SELECT userId FROM provinces WHERE id=(?)", (province_id,)).fetchone()[0]
+
+            unit_amount = db.execute(f"SELECT {unit} FROM proInfra WHERE id=(?)", (province_id,)).fetchone()[0]
+
+            if unit_amount == 0:
+                continue
+            else:
+                ### REMOVING RESOURCE
+
+                plus_amount *= unit_amount
+                operating_costs *= unit_amount
+                if pollution_amount != None:
+                    pollution_amount *= unit_amount
+                    
+                ### ADDING ENERGY
+                current_plus_resource = db.execute(f"SELECT {plus_resource} FROM provinces WHERE id=(?)", (user_id,)).fetchone()[0]
+                new_resource_number = current_plus_resource + plus_amount # 12 is how many uranium it generates
+                db.execute(f"UPDATE provinces SET {plus_resource}=(?) WHERE id=(?)", (new_resource_number, user_id))
+                ### REMOVING MONEY
+                current_money = int(db.execute("SELECT gold FROM stats WHERE id=(?)", (user_id,)).fetchone()[0])
+                if current_money < operating_costs:
+                    continue
+                else:
+                    new_money = current_money - operating_costs
+                    db.execute("UPDATE stats SET gold=(?) WHERE id=(?)", (new_money, user_id))
+                    if pollution_amount != None:
+                        current_pollution = db.execute("SELECT pollution FROM provinces WHERE id=(?)", (province_id,)).fetchone()[0]
+                        new_pollution = current_pollution + pollution_amount
+                        db.execute("UPDATE provinces SET pollution=(?) WHERE id=(?)", (new_pollution, province_id))
+
+        conn.commit()
+
+    conn.close()
+
+generate_province_revenue()
 
