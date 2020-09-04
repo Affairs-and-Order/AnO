a1065d661861b0dac6353167f74698586ca35a70
Steven Kim <97stevenkim@gmail.com>
2020-09-04 15:44:07 -0400

    initial javascript commit for 3 checks

--- static/script.js
+++ static/script.js
@@ -590,10 +590,13 @@ $(document).ready(function() {
 
 $(document).ready(function() {
     var limit = 3;
-    $('input.warcheckbox').on('change', function(evt) {
+    $('input.warcheckbox').on('change', function (evt) {
+        let checked = $(this).siblings(':checked').length;
         if ($(this).siblings(':checked').length >= limit) {
             this.checked = false;
         }
+        let checkElement = document.getElementById("chosenAmountChecked");
+        checkElement.innerHTML = $(this).siblings(':checked').length + " / 3";
     });
 });
 
