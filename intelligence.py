58181f1158405aed6f404fe44b1c67f0108b33e6
Steven Kim <97stevenkim@gmail.com>
2020-09-17 20:48:31 -0400

    tuple ename unpacking

--- intelligence.py
+++ intelligence.py
@@ -16,15 +16,25 @@ def intelligence():
         connection = sqlite3.connect("affo/aao.db")
         db = connection.cursor()
         cId = session["user_id"]
+        print(cId)
         yourCountry = db.execute(
             "SELECT username FROM users WHERE id=(?)", (cId,)).fetchone()[0]
 
         units = 37894562385
-        emptyCountryDict = {'soldiers': 'Unknown', 'tanks': 'Unknown', 'artillery': 'Unknown', 'bombers': 'Unknown', 'fighters': 'Unknown', 'apaches': 'Unknown',
+        emptyCountryDict = {'eName': 'placeholder', 'soldiers': 'Unknown', 'tanks': 'Unknown', 'artillery': 'Unknown', 'bombers': 'Unknown', 'fighters': 'Unknown', 'apaches': 'Unknown',
                             'destroyers': 'Unknown', 'cruisers': 'Unknown', 'submarines': 'Unknown', 'spies': 'Unknown', 'icbms': 'Unknown', 'nukes': 'Unknown'}
         # retrieve all entries from the spy table where spyer = cId
-        # for each entry make a 'enemyNation': emptyCountryDict and add to a list of spyEntries
+
+        spyinfodb = db.execute("SELECT * FROM spyinfo WHERE spyer=(?)", (cId,)).fetchall()
+        print(spyinfodb)
         spyEntries = []
+        for tuple in spyinfodb:
+            spyEntries.append(emptyCountryDict)
+            spyEntries['eName'] = db.execute("SELECT username FROM users WHERE id=(?)", (tuple[2],)).fetchone()[0]
+        
+        # for each entry make a 'enemyNation': emptyCountryDict and add to a list of spyEntries
+
         db.close()
         connection.close()
         return render_template("intelligence.html", units=units, yourCountry=yourCountry, enemyCountry="enemyCountry")
+    return 'hi'
