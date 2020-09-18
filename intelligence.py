3b65cf6de0330d94b149d0f4982a7f7455257839
Steven Kim <97stevenkim@gmail.com>
2020-09-17 20:18:39 -0400

    spyentries jinja

--- intelligence.py
+++ intelligence.py
@@ -11,19 +11,20 @@ import time
 @login_required
 @app.route("/intelligence", methods=["GET", "POST"])
 def intelligence():
-    connection = sqlite3.connect("affo/aao.db")
-    db = connection.cursor()
-    cId = session["user_id"]
 
     if request.method == "GET":
+        connection = sqlite3.connect("affo/aao.db")
+        db = connection.cursor()
+        cId = session["user_id"]
         yourCountry = db.execute(
             "SELECT username FROM users WHERE id=(?)", (cId,)).fetchone()[0]
-        db.close()
-        connection.close()
-        units = 234234
-        emptyCountryDict = {'soldiers': 'Unknown', 'tanks': 'Unknown', 'artillery': 'Unknown', 'bombers': 'Unknown', 'fighters': 'Unknown', 'apaches': 'Unknown', 'destroyers': 'Unknown', 'cruisers': 'Unknown', 'submarines': 'Unknown', 'spies': 'Unknown', 'icbms': 'Unknown', 'nukes': 'Unknown'}
+
+        units = 37894562385
+        emptyCountryDict = {'soldiers': 'Unknown', 'tanks': 'Unknown', 'artillery': 'Unknown', 'bombers': 'Unknown', 'fighters': 'Unknown', 'apaches': 'Unknown',
+                            'destroyers': 'Unknown', 'cruisers': 'Unknown', 'submarines': 'Unknown', 'spies': 'Unknown', 'icbms': 'Unknown', 'nukes': 'Unknown'}
         # retrieve all entries from the spy table where spyer = cId
         # for each entry make a 'enemyNation': emptyCountryDict and add to a list of spyEntries
         spyEntries = []
-        # units is a dictionary of dictionaries. {eName: {}}
+        db.close()
+        connection.close()
         return render_template("intelligence.html", units=units, yourCountry=yourCountry, enemyCountry="enemyCountry")
