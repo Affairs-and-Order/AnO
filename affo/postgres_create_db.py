28b8c9d68d062cdbb971dbc11c314b71f54a540f
delivey <deliveydev@gmail.com>
2020-10-15 16:11:42 +0300

    more postgres stuff

--- affo/postgres_create_db.py
+++ affo/postgres_create_db.py
@@ -1,11 +1,15 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv()
     
 connection = psycopg2.connect(
-    database="ano",
-    user="postgres",
-    password="q",
-    host="localhost",
-    port="5432")
+    database=os.getenv("PG_DATABASE"),
+    user=os.getenv("PG_USER"),
+    password=os.getenv("PG_PASSWORD"),
+    host=os.getenv("PG_HOST"),
+    port=os.getenv("PG_PORT"))
+    
 db = connection.cursor()
 
 tables = [
