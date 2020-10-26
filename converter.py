05d0a9debca2fca24504534f7b9ca991958d50d1
delivey <deliveydev@gmail.com>
2020-10-26 18:55:00 +0200

    addded basic sqlite3 statement converter

---
+++ converter.py
@@ -0,0 +1,48 @@
+file = 'tests.py'
+output = open('converted.py', "w")
+
+files = ["app1.py"]
+
+with open(file) as f:
+    for line in f.readlines():
+        if 'import sqlite3' in line:
+            new_line = line.replace('import sqlite3', 'import psycopg2')
+            output.write(new_line)
+        elif "connection = sqlite3.connect('affo/aao.db')" in line:
+            new_line = line.replace("connection = sqlite3.connect('affo/aao.db')",
+"""
+connection = psycopg2.connect(
+    database=os.getenv("PG_DATABASE"),
+    user=os.getenv("PG_USER"),
+    password=os.getenv("PG_PASSWORD"),
+    host=os.getenv("PG_HOST"),
+    port=os.getenv("PG_PORT"))
+""")
+            output.write(new_line)
+        elif 'connection = sqlite3.connect("affo/aao.db")' in line:
+            new_line = line.replace('connection = sqlite3.connect("affo/aao.db")',
+"""
+connection = psycopg2.connect(
+    database=os.getenv("PG_DATABASE"),
+    user=os.getenv("PG_USER"),
+    password=os.getenv("PG_PASSWORD"),
+    host=os.getenv("PG_HOST"),
+    port=os.getenv("PG_PORT"))
+""")
+            output.write(new_line)
+
+        elif '?' in line and 'db.execute' in line:
+            new_line = line.replace('?', '%s')
+            if '.fetch' in line:
+                fetching = '.fetch' + line.split(".fetch", 1)[1]
+                new_line = new_line.replace(fetching, "")
+            if '=' in line:
+                variable = line.partition("db.execute(")[0]
+                new_line = new_line.replace(variable, "")
+
+            full_cursor = variable + 'db' + fetching
+            output.write(new_line)
+            output.write(f'\n{full_cursor}')
+        else:
+            output.write(line)
+        
