8d5ba6f0e23bb6cb0208735a8fa1666d49d73877
delivey <deliveydev@gmail.com>
2020-10-27 11:27:26 +0200

    converted coalitions

--- converter.py
+++ converter.py
@@ -1,34 +1,13 @@
-import os
+file = 'coalitions1.py'
+output = open('coalitions.py', "w")
 
-file = 'tests.py'
-
-files = ["app.py", "coalitions.py", "countries.py", "formula.py",
-"helpers.py", "intelligence.py", "login.py", "market.py", "military.py",
-"province.py", "signup.py", "testing.py", "testroutes.py", "units.py",
-"upgrades.py", "wars.py"]
-
-new_files = []
-
-for filename in files:
-    substr = ".py"
-    inserttxt = "1"
-
-    idx = filename.index(substr)
-    new_filename = filename[:idx] + inserttxt + filename[idx:]
-    os.rename(filename, new_filename)
-    new_files.append(new_filename)
-
-all_files = zip(new_files, files)
-
-for new_file, old_file in all_files:
-    output = open(old_file, "w")
-    with open(new_file) as f:
-        for line in f.readlines():
-            if 'import sqlite3' in line:
-                new_line = line.replace('import sqlite3', 'import psycopg2')
-                output.write(new_line)
-            elif "connection = sqlite3.connect('affo/aao.db')" in line:
-                new_line = line.replace("connection = sqlite3.connect('affo/aao.db')",
+with open(file) as f:
+    for line in f.readlines():
+        if 'import sqlite3' in line:
+            new_line = line.replace('import sqlite3', 'import psycopg2')
+            output.write(new_line)
+        elif "connection = sqlite3.connect('affo/aao.db')" in line:
+            new_line = line.replace("connection = sqlite3.connect('affo/aao.db')",
 """
 connection = psycopg2.connect(
     database=os.getenv("PG_DATABASE"),
@@ -37,34 +16,31 @@ connection = psycopg2.connect(
     host=os.getenv("PG_HOST"),
     port=os.getenv("PG_PORT"))
 """)
-                output.write(new_line)
-            elif 'connection = sqlite3.connect("affo/aao.db")' in line:
-                new_line = line.replace('connection = sqlite3.connect("affo/aao.db")',
+            output.write(new_line)
+        elif 'connection = sqlite3.connect("affo/aao.db")' in line:
+            new_line = line.replace('connection = sqlite3.connect("affo/aao.db")',
 """
-    connection = psycopg2.connect(
-        database=os.getenv("PG_DATABASE"),
-        user=os.getenv("PG_USER"),
-        password=os.getenv("PG_PASSWORD"),
-        host=os.getenv("PG_HOST"),
-        port=os.getenv("PG_PORT"))
+connection = psycopg2.connect(
+    database=os.getenv("PG_DATABASE"),
+    user=os.getenv("PG_USER"),
+    password=os.getenv("PG_PASSWORD"),
+    host=os.getenv("PG_HOST"),
+    port=os.getenv("PG_PORT"))
 """)
-                output.write(new_line)
+            output.write(new_line)
 
-            elif '?' in line and 'db.execute' in line:
-                new_line = line.replace('?', '%s')
-                if '.fetch' in line:
-                    fetching = '.fetch' + line.split(".fetch", 1)[1]
-                    new_line = new_line.replace(fetching, "")
-                if '=' in line:
-                    variable = line.partition("db.execute(")[0]
-                    new_line = new_line.replace(variable, "")
+        elif '?' in line and 'db.execute' in line:
+            new_line = line.replace('?', '%s')
+            if '.fetch' in line:
+                fetching = '.fetch' + line.split(".fetch", 1)[1]
+                new_line = new_line.replace(fetching, "")
+            if '=' in line:
+                variable = line.partition("db.execute(")[0]
+                new_line = new_line.replace(variable, "")
 
-                try:
-                    full_cursor = variable + 'db' + fetching
-                except: 
-                    full_cursor = variable + 'db'
-
-                output.write(new_line)
-                output.write(f'\n{full_cursor}')
-            else:
-                output.write(line)
+            full_cursor = variable + 'db' + fetching
+            output.write(new_line)
+            output.write(f'\n{full_cursor}')
+        else:
+            output.write(line)
+        
