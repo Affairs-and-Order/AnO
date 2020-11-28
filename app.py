acaac75d3bd28aa1d30249691e5bdcb96d616826
delivey <deliveydev@gmail.com>
2020-11-28 12:10:31 +0200

    server stuff

--- app.py
+++ app.py
@@ -292,6 +292,5 @@ def warresult():
 def mass_purchase():
     return render_template("mass_purchase.html")
 
-# available to run if double click the file
 if __name__ == "__main__":
-    app.run(debug=True) # Runs the app with debug mode on
+    app.run(host='0.0.0.0')
