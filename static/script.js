a4575d3a5ab679d86ac7b0634c348e36a7b803f8
Carson Burke <carsonburke22@gmail.com>
2020-12-16 17:48:45 -0800

    added image slider back to loginsignupdiv

--- static/script.js
+++ static/script.js
@@ -649,3 +649,26 @@ function war_target() {
         }
     }
 }
+//Code for image slider in signup page
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slideparent");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
