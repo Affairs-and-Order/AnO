896286fe90db4405c97272d2c0afd9d49092408f
delivey <deliveydev@gmail.com>
2021-01-04 14:19:31 +0200

    added lumber to places where it was missing

---
+++ variables.py
@@ -0,0 +1,8 @@
+# File for variables that are repeated multiple times in other files (for example, the resources list)
+
+RESOURCES = [
+    "rations", "oil", "coal", "uranium", "bauxite", "lead", "copper", "iron",
+    "lumber", "components", "steel", "consumer_goods", "aluminium",
+    "gasoline", "ammunition"
+]
+
