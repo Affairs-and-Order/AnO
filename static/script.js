514b3292ac9bf3f74da3025e04ea50295567d03e
Carson Burke <carsonburke22@gmail.com>
2021-01-08 20:08:01 -0800

    fixed /signup /login and loading animations

--- static/script.js
+++ static/script.js
@@ -18,28 +18,7 @@ ATTEMPT TO MAKE RESOURCEBAR STAY OPEN ON REFRESH
         }
     });
     */
-//SCROLLBAR ANIMATION
-window.onscroll = function() { scrolltop() };
 
-function scrolltop() {
-
-    if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-
-        document.getElementById("navbar").style.backgroundColor = "#363945";
-        document.getElementById("navbar").style.boxShadow = "box-shadow: 0 0 0.5rem 0 rgba(58, 59, 69, 0)";
-
-        document.getElementById("navbar").style.transition = "all 0.2s";
-
-    } else {
-
-        document.getElementById("navbar").style.backgroundColor = "transparent";
-        document.getElementById("navbar").style.boxShadow = "box-shadow: 0 0 0.5rem 0 rgba(58, 59, 69, 0.8)";
-
-        document.getElementById("navbar").style.transition = "all 0.2s";
-
-    }
-}
-AOS.init();
 //navbar events
 function menubardrop() {
 
