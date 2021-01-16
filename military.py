50c8f9c819fc6e5dc9bc542bf3e478b0eb831e1b
Carson Burke <carsonburke22@gmail.com>
2021-01-15 22:23:59 -0800

    fixed soldiers

--- military.py
+++ military.py
@@ -72,7 +72,6 @@ def military_sell_buy(way, units):  # WARNING: function used only for military
             ## LAND
 
             "soldiers_price": 200, # Cost 200
-            "soldiers_resource": {"rations": 2},
             "soldiers_manpower": 1,
 
             "tanks_price": 8000, # Cost 8k
