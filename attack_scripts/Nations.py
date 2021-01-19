ae300ca3a6aa3584e5ca25e811a123aa19b5578d
marter11 <>
2021-01-19 21:55:40 +0200

    /military changes...

--- attack_scripts/Nations.py
+++ attack_scripts/Nations.py
@@ -168,8 +168,18 @@ class Nation:
         pass
 
     # Function for sending posts to nation's news page
-    def news_signal(message):
-        pass
+    @staticmethod
+    def send_news(destination_id: int, message: str):
+        connection = psycopg2.connect(
+            database=os.getenv("PG_DATABASE"),
+            user=os.getenv("PG_USER"),
+            password=os.getenv("PG_PASSWORD"),
+            host=os.getenv("PG_HOST"),
+            port=os.getenv("PG_PORT"))
+        db = connection.cursor()
+        db.execute("INSERT INTO news(destination_id, message) VALUES (%s, %s)", (destination_id, message))
+        connection.commit()
+        connection.close()
 
     def get_provinces(self):
 
@@ -852,22 +862,22 @@ class Military(Nation):
 
         # TODO: maybe clear this mess a bit up
         # Land units
-        if army_bases*100 > manpower:
+        if army_bases*100 >= manpower:
             soldiers = manpower
         elif (army_bases*100)-military["soldiers"] > 0:
             soldiers = (army_bases*100)-military["soldiers"]
         else:
             soldiers = 0
 
-        if army_bases*8 > manpower//4:
+        if army_bases*8 >= manpower//4:
             tanks = manpower//4
         elif (army_bases*8)-military["tanks"] > 0:
             tanks = (army_bases*8)-military["tanks"]
         else:
             tanks = 0
 
-        if army_bases*8 > manpower//2:
-            artillery = army_bases//2
+        if army_bases*8 >= manpower//2:
+            artillery = manpower//2
         elif (army_bases*8)-military["artillery"] > 0:
             artillery = (army_bases*8)-military["artillery"]
         else:
@@ -875,7 +885,7 @@ class Military(Nation):
 
         # Air units
         air_units = military["fighters"]+military["bombers"]+military["apaches"]
-        if aerodomes*5 > manpower:
+        if aerodomes*5 >= manpower:
             bombers = manpower
             fighters = manpower
             apaches = manpower
@@ -889,7 +899,7 @@ class Military(Nation):
             fighters = 0
 
         # Naval units
-        if harbours*3 > manpower//6:
+        if harbours*3 >= manpower//6:
             submarines = manpower//6
             destroyers = manpower//6
         elif (harbours*3)-military["submarines"]-military["destroyers"] > 0:
@@ -899,7 +909,7 @@ class Military(Nation):
             destroyers = 0
             submarines = 0
 
-        if harbours*2 > manpower//5:
+        if harbours*2 >= manpower//5:
             cruisers = manpower//5
         elif (harbours*2)-military["cruisers"] > 0:
             cruisers = (harbours*2)-military["cruisers"]
@@ -1000,5 +1010,7 @@ if __name__ == "__main__":
     # Military.infrastructure_damage(20, p)
     # print(p)
 
-    m = Military(2)
-    m.reparation_tax([2], [1])
+    # m = Military(2)
+    # m.reparation_tax([2], [1])
+
+    Nation.send_news(2, "You won the 100 years war!")
