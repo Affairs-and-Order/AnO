c6df2fe7bc2758df247cbc24de32f92825a765a9
marter11 <38880056+marter11@users.noreply.github.com>
2021-02-24 14:15:47 -0500

    added news for initialization

--- affo/create_db.py
+++ affo/create_db.py
@@ -16,7 +16,7 @@ tables = [
     "coalitions", "colBanks", "colBanksRequests", "colNames",
     "keys", "military", "offers", "proInfra", "provinces", "upgrades",
     "requests", "resources", "spyinfo", "stats", "trades",
-    "treaties", "users", "peace", "wars", "reparation_tax"
+    "treaties", "users", "peace", "wars", "reparation_tax", "news"
 ]
 
 
