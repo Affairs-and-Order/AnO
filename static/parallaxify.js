33b7ea6768d0c2857f6bc46db583fe573c0705f9
Carson Burke <carsonburke22@gmail.com>
2021-06-24 11:07:38 -0700

    added parallax to landing parent

---
+++ static/parallaxify.js
@@ -0,0 +1,44 @@
+window.addEventListener("scroll", parallax)
+
+function parallax() {
+
+    function getPosition(item) {
+
+        let top = (item.getBoundingClientRect().top / window.innerHeight * 100).toFixed(0)
+
+        return top
+    }
+
+    let itemsToParallax = document.getElementsByClassName("parallaxify")
+
+    for (let item of itemsToParallax) {
+
+        if (!item.dataset.speedMultiplier) {
+
+            item.dataset.speedMultiplier = "0.7"
+        }
+        item.style.position = "relative"
+
+        item.style.top = getPosition(item) * item.dataset.speedMultiplier + "px"
+    }
+
+    let imagesToParallax = document.getElementsByClassName("parallaxifyBg")
+
+    for (let item of imagesToParallax) {
+
+        function getPosition(item) {
+
+            let top = (item.getBoundingClientRect().top / window.innerHeight * 100).toFixed(0)
+
+            return top
+        }
+
+        if (!item.dataset.speedMultiplier) {
+
+            item.dataset.speedMultiplier = "3"
+        }
+
+        item.style.backgroundAttachment = "fixed"
+        item.style.backgroundPositionY = getPosition(item) * item.dataset.speedMultiplier + "px"
+    }
+}
