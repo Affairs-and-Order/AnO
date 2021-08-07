fbcd810938f61e7bad7437f746c81f2c894a3bad
delivey <deliveydev@gmail.com>
2021-08-07 13:41:34 +0300

    more building balancing changes

--- variables.py
+++ variables.py
@@ -68,24 +68,24 @@ INFRA = {
     'oil_burners_effect': [{'pollution': 5}],
 
     'hydro_dams_plus': {'energy': 6},
-    'hydro_dams_money': 31200,
+    'hydro_dams_money': 24000,
 
-    'nuclear_reactors_plus': {'energy': 20}, 
+    'nuclear_reactors_plus': {'energy': 15}, 
     'nuclear_reactors_convert_minus': [{'uranium': 32}],
-    'nuclear_reactors_money': 54600, 
+    'nuclear_reactors_money': 111000, 
 
     'solar_fields_plus': {'energy': 4},
-    'solar_fields_money': 18000, 
+    'solar_fields_money': 11000, 
     ####################
 
     ### Retail ### (Done)
-    'gas_stations_plus': {'consumer_goods': 10},
+    'gas_stations_plus': {'consumer_goods': 12},
     'gas_stations_effect': [{'pollution': 4}],
     'gas_stations_money': 20000,
 
-    'general_stores_plus': {'consumer_goods': 14},
+    'general_stores_plus': {'consumer_goods': 10},
     'general_stores_effect': [{'pollution': 2}],
-    'general_stores_money': 35000, 
+    'general_stores_money': 37500, 
 
     'farmers_markets_plus': {'consumer_goods': 16},
     'farmers_markets_effect': [{'pollution': 5}],
@@ -108,14 +108,14 @@ INFRA = {
     'libraries_money': 60000,
 
     'hospitals_effect': {'happiness': 8},   
-    'hospitals_money': 90000,
+    'hospitals_money': 85000,
 
     'universities_effect': [{'productivity': 10}, {'happiness': 4}],
     'universities_money': 175000,
 
     'monorails_effect': [{'productivity': 16}],
     'monorails_effect_minus': {'pollution': 20},
-    'monorails_money': 500000,
+    'monorails_money': 270000,
     ###################
 
     ### Military (Done) ###
@@ -129,66 +129,66 @@ INFRA = {
     ### Industry (Done) ###
 
     'farms_money': 3000, # Costs $3k
-    'farms_plus': {'rations': 2},
+    'farms_plus': {'rations': 20},
     'farms_effect': [{'pollution': 1}],
 
-    'pumpjacks_money': 10000, # Costs $10k
-    'pumpjacks_plus': {'oil': 23},
-    'pumpjacks_effect': [{'pollution': 1.5}],
+    'pumpjacks_money': 9500,
+    'pumpjacks_plus': {'oil': 25},
+    'pumpjacks_effect': [{'pollution': 2}],
 
-    'coal_mines_money': 10000, # Costs $10k
-    'coal_mines_plus': {'coal': 26},
+    'coal_mines_money': 4200, # Costs $10k
+    'coal_mines_plus': {'coal': 25},
     'coal_mines_effect': [{'pollution': 2}],
 
     'bauxite_mines_money': 8000, # Costs $8k
     'bauxite_mines_plus': {'bauxite': 20},
-    'bauxite_mines_effect': [{'pollution': 1.5}],
+    'bauxite_mines_effect': [{'pollution': 2}],
 
-    'copper_mines_money': 8000, # Costs $8k
-    'copper_mines_plus': {'copper': 32},
-    'copper_mines_effect': [{'pollution': 1.5}],
+    'copper_mines_money': 5000, # Costs $8k
+    'copper_mines_plus': {'copper': 25},
+    'copper_mines_effect': [{'pollution': 2}],
 
-    'uranium_mines_money': 18000, # Costs $18k
+    'uranium_mines_money': 45000, # Costs $18k
     'uranium_mines_plus': {'uranium': 12},
     'uranium_mines_effect': [{'pollution': 1}],
 
-    'lead_mines_money': 12000,
-    'lead_mines_plus': {'lead': 18},
-    'lead_mines_effect': [{'pollution': 1.5}],
+    'lead_mines_money': 7200,
+    'lead_mines_plus': {'lead': 16},
+    'lead_mines_effect': [{'pollution': 2}],
 
-    'iron_mines_money': 12000,
+    'iron_mines_money': 11000,
     'iron_mines_plus': {'iron': 25},
-    'iron_mines_effect': [{'pollution': 1.5}],
+    'iron_mines_effect': [{'pollution': 2}],
 
     'lumber_mills_money': 7500,
-    'lumber_mills_plus': {'lumber': 32},
+    'lumber_mills_plus': {'lumber': 30},
     'lumber_mills_effect': [{'pollution': 1}],
 
     ################
 
     ### Processing (Done) ###
-    'component_factories_money': 220000, # Costs $220k
+    'component_factories_money': 80000, # Costs $220k
     'component_factories_convert_minus': [{'copper': 20}, {'steel': 10}, {'aluminium': 15}],
     'component_factories_plus': {'components': 5},
     'component_factories_effect': [{'pollution': 5}],
 
-    'steel_mills_money': 180000,
+    'steel_mills_money': 90000,
     'steel_mills_convert_minus': [{'coal': 35}, {'iron': 35}],
-    'steel_mills_plus': {'steel': 15},
+    'steel_mills_plus': {'steel': 12},
     'steel_mills_effect': [{'pollution': 4}],
 
-    'ammunition_factories_money': 140000,
+    'ammunition_factories_money': 30000,
     'ammunition_factories_convert_minus': [{'copper': 10}, {'lead': 20}],
-    'ammunition_factories_plus': {'ammunition': 10},
+    'ammunition_factories_plus': {'ammunition': 12},
     'ammunition_factories_effect': [{'pollution': 3}],
 
-    'aluminium_refineries_money': 150000,
+    'aluminium_refineries_money': 72000,
     'aluminium_refineries_convert_minus': [{'bauxite': 15}],
-    'aluminium_refineries_plus': {'aluminium': 12},
+    'aluminium_refineries_plus': {'aluminium': 16},
     'aluminium_refineries_effect': [{'pollution': 3}],
 
-    'oil_refineries_money': 160000,
+    'oil_refineries_money': 55000,
     'oil_refineries_convert_minus': [{'oil': 20}],
-    'oil_refineries_plus': {'gasoline': 8},
+    'oil_refineries_plus': {'gasoline': 11},
     'oil_refineries_effect': [{'pollution': 6}]
 }
