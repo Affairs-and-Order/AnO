9df0cbaa70f4879e223a84245398db68963ad27c
delivey <deliveydev@gmail.com>
2021-10-07 16:50:03 +0300

    updated policy selecting to arrays so multiple can be selected

--- policies.py
+++ policies.py
@@ -5,6 +5,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# Getting all the policy numbers from a request form
+def get_policies(type, prange, form):
+    policies = []
+    for i in range(1, prange+1):
+        value = form.get(f"{type}{i}")
+        if (value != None): policies.append(int(value))
+    return policies
+
 @app.route("/policies/update", methods=["POST"])
 def policies():
 
@@ -19,10 +27,10 @@ def policies():
 
     db = conn.cursor()
 
-    military = request.form.get("military")
-    education = request.form.get("education")
+    military = get_policies("soldiers", 7, request.form)
+    education = get_policies("education", 6, request.form)
 
-    db.execute("UPDATE policies SET military=%s WHERE user_id=%s", (military, cId))
+    db.execute("UPDATE policies SET soldiers=%s WHERE user_id=%s", (military, cId))
     db.execute("UPDATE policies SET education=%s WHERE user_id=%s", (education, cId))
 
     conn.commit()
