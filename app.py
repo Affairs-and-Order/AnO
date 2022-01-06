751ce55413950a9caaf59cadf7182c5929594810
delivey <deliveydev@gmail.com>
2022-01-06 20:28:56 +0200

    Removal of trumbowyg code.

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import random
 import string
 from datetime import datetime
 from psycopg2.extras import RealDictCursor
+import mimetypes
+mimetypes.add_type('image/svg+xml', '.svg')
 
 app = Flask(__name__)
 
