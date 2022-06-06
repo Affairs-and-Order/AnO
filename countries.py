4853a152c423182bdcdb30d8bcce7bc6b4c68444
delivey <deliveydev@gmail.com>
2022-06-06 20:45:00 +0300

    Big optimization of get_econ_statistics

--- countries.py
+++ countries.py
@@ -15,10 +15,12 @@ from datetime import datetime
 from wars import target_data
 load_dotenv()
 
+from psycopg2.extras import RealDictCursor
+
 app.config['UPLOAD_FOLDER'] = 'static/flags'
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024  # 2 Mb limit
 
-
+# TODO: rewrite this function for fucks sake
 def get_econ_statistics(cId):
 
     conn = psycopg2.connect(
@@ -28,95 +30,52 @@ def get_econ_statistics(cId):
         host=os.getenv("PG_HOST"),
         port=os.getenv("PG_PORT"))
 
-    db = conn.cursor()
+    dbdict = conn.cursor(cursor_factory=RealDictCursor)
 
-    db.execute("SELECT id FROM provinces WHERE userId=%s", (cId,))
-    provinces_list = db.fetchall()
-    provinces = []
-
-    for pId in provinces_list:
-        provinces.append(pId[0])
-
-    provinces = tuple(provinces)
-
-    total = {}
-    total = defaultdict(lambda: 0, total)
-
-    # This is a pretty bad way of doing this, but the best
-    # I thought of in the time given
-    # TODO: find way to do this that would take less LOC
+    # TODO: less loc
     try:
-        db.execute(
-            """
+        dbdict.execute(
+        """
         SELECT
-        coal_burners, oil_burners, hydro_dams, nuclear_reactors, solar_fields,
-        gas_stations, general_stores, farmers_markets, malls, banks,
-        city_parks, hospitals, libraries, universities, monorails,
-        army_bases, harbours, aerodomes, admin_buildings, silos,
-        farms, pumpjacks, coal_mines, bauxite_mines,
-        copper_mines, uranium_mines, lead_mines, iron_mines,
-        lumber_mills, component_factories, steel_mills, ammunition_factories,
-        aluminium_refineries, oil_refineries
-        FROM proInfra WHERE id IN %s
-        """, (provinces,))
-        province_units = db.fetchall()
+        SUM(proInfra.coal_burners) AS coal_burners,
+        SUM(proInfra.oil_burners) AS oil_burners,
+        SUM(proInfra.hydro_dams) AS hydro_dams ,
+        SUM(proInfra.nuclear_reactors) AS nuclear_reactors,
+        SUM(proInfra.solar_fields) AS solar_fields,
+        SUM(proInfra.gas_stations) AS gas_stations,
+        SUM(proInfra.general_stores) AS general_stores,
+        SUM(proInfra.farmers_markets) AS farmers_markets,
+        SUM(proInfra.malls) AS malls,
+        SUM(proInfra.banks) AS banks,
+        SUM(proInfra.city_parks) AS city_parks,
+        SUM(proInfra.hospitals) AS hospitals,
+        SUM(proInfra.libraries) AS libraries,
+        SUM(proInfra.universities) AS universities,
+        SUM(proInfra.monorails) AS monorails,
+        SUM(proInfra.army_bases) AS army_bases,
+        SUM(proInfra.harbours) AS harbours,
+        SUM(proInfra.aerodomes) AS aerodomes,
+        SUM(proInfra.admin_buildings) AS admin_buildings,
+        SUM(proInfra.silos) AS silos,
+        SUM(proInfra.farms) AS farms,
+        SUM(proInfra.pumpjacks) AS pumpjacks,
+        SUM(proInfra.coal_mines) AS coal_mines,
+        SUM(proInfra.bauxite_mines) AS bauxite_mines,
+        SUM(proInfra.copper_mines) AS copper_mines,
+        SUM(proInfra.uranium_mines) AS uranium_mines,
+        SUM(proInfra.lead_mines) AS lead_mines,
+        SUM(proInfra.iron_mines) AS iron_mines,
+        SUM(proInfra.lumber_mills) AS lumber_mills,
+        SUM(proInfra.component_factories) AS component_factories,
+        SUM(proInfra.steel_mills) AS steel_mills,
+        SUM(proInfra.ammunition_factories) AS ammunition_factories,
+        SUM(proInfra.aluminium_refineries) AS aluminium_refineries,
+        SUM(proInfra.oil_refineries) AS oil_refineries
+        FROM proInfra LEFT JOIN provinces ON provinces.id=proInfra.id WHERE provinces.userId=%s;
+        """, (cId,))
+        total = dict(dbdict.fetchone())
     except:
-        province_units = []
-
-    for units in province_units:
-
-        units = list(units)
-
-        coal_burners, oil_burners, hydro_dams, nuclear_reactors, solar_fields, \
-            gas_stations, general_stores, farmers_markets, malls, banks, \
-            city_parks, hospitals, libraries, universities, monorails, \
-            army_bases, harbours, aerodomes, admin_buildings, silos, \
-            farms, pumpjacks, coal_mines, bauxite_mines, \
-            copper_mines, uranium_mines, lead_mines, iron_mines, \
-            lumber_mills, component_factories, steel_mills, ammunition_factories, \
-            aluminium_refineries, oil_refineries = units
-
-        total["coal_burners"] += coal_burners
-        total["oil_burners"] += oil_burners
-        total["hydro_dams"] += hydro_dams
-        total["nuclear_reactors"] += nuclear_reactors
-        total["solar_fields"] += solar_fields
-
-        total["gas_stations"] += gas_stations
-        total["general_stores"] += general_stores
-        total["farmers_markets"] += farmers_markets
-        total["malls"] += malls
-        total["banks"] += banks
-
-        total["city_parks"] += city_parks
-        total["hospitals"] += hospitals
-        total["libraries"] += libraries
-        total["universities"] += universities
-        total["monorails"] += monorails
-
-        total["army_bases"] += army_bases
-        total["harbours"] += harbours
-        total["aerodomes"] += aerodomes
-        total["admin_buildings"] += admin_buildings
-        total["silos"] += silos
-
-        total["farms"] += farms
-        total["pumpjacks"] + pumpjacks
-        total["coal_mines"] += coal_mines
-        total["bauxite_mines"] += bauxite_mines
-
-        total["copper_mines"] += copper_mines
-        total["uranium_mines"] += uranium_mines
-        total["lead_mines"] += lead_mines
-        total["iron_mines"] += iron_mines
-
-        total["lumber_mills"] += lumber_mills
-        total["component_factories"] += component_factories
-        total["steel_mills"] += steel_mills
-        total["ammunition_factories"] += ammunition_factories
-
-        total["aluminium_refineries"] += aluminium_refineries
-        total["oil_refineries"] += oil_refineries
+        total = {}
 
     expenses = {}
     expenses = defaultdict(lambda: defaultdict(lambda: 0), expenses)
