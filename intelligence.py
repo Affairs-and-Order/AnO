4b998888727a84aa34e0cfe1cbfe6ea3eba39bf8
delivey <deliveydev@gmail.com>
2022-06-15 20:13:52 +0300

    Spying scores and start of showing system

--- intelligence.py
+++ intelligence.py
@@ -2,7 +2,7 @@
 
 from app import app
 from flask import request, render_template, session, redirect
-from helpers import login_required
+from helpers import login_required, error
 from attack_scripts import Military
 import time
 from random import random
@@ -10,6 +10,8 @@ import psycopg2
 import os
 from dotenv import load_dotenv
 import variables
+import random as rand
+import time
 load_dotenv()
 
 @app.route("/intelligence", methods=["GET"])
@@ -74,7 +76,6 @@ def intelligence():
                 # commented so we dont delete test spyentries
                 # return "enemy nation doesn't exist"
 
-        db.close()
         connection.close()
         return render_template("intelligence.html", yourCountry=yourCountry, spyEntries=spyEntries)
 
@@ -139,6 +140,7 @@ def spyAmount():
         return redirect("/spyResult")
 
 
+# TODO: add notifications
 @app.route("/spyResult", methods=["GET", "POST"])
 @login_required
 def spyResult():
@@ -148,8 +150,9 @@ def spyResult():
         return render_template("spyResult.html", enemyNation=enemyNation, spyEntry=spyEntry)
     if request.method == "POST":
 
-        country = request.form.get("country")
-        spies = request.form.get("spies")
+        cId = session["user_id"]
+        eId = request.form.get("country")
+        spies = int(request.form.get("spies"))
         spy_type = request.form.get("spy_type")
 
         connection = psycopg2.connect(
@@ -160,3 +163,68 @@ def spyResult():
             port=os.getenv("PG_PORT"))
 
         db = connection.cursor()
+
+        db.execute("SELECT spies FROM military WHERE id=%s", (cId,))
+        actual_spies = db.fetchone()[0]
+
+        if spies > actual_spies:
+            return error(400, f"You don't have enough spies ({spies}/{actual_spies}). Missing {actual_spies-spies} spies")
+
+
+        db.execute("SELECT spies FROM military WHERE id=%s", (eId,))
+        enemy_spies = db.fetchone()[0]
+
+        print(eId, enemy_spies)
+
+        executed_spies = 0 # ADD NOTIFICATION FOR THIS
+        uncovered_spies = 0 # ADD NOTIFICATION FOR THIS
+        uncovered = {}
+
+        if spy_type == "resources":
+            for resource in variables.RESOURCES:
+                own_rand = round(rand.uniform(0, 1), 3)
+                enemy_rand = round(rand.uniform(0, 1), 3)
+
+                own_score = own_rand * spies
+                enemy_score = enemy_rand * enemy_spies
+
+                if own_score == 0: own_score = 0.0001
+                if enemy_score == 0: enemy_score = 0.0001
+
+                multiplier = enemy_score / own_score
+
+                if multiplier > 10:
+                    executed_spies += 1
+                if multiplier > 2:
+                    uncovered_spies += 1
+                if multiplier > 1: # Enemy won
+                    uncovered[resource] = False
+                elif multiplier <= 1: # Own won
+                    uncovered[resource] = True
+
+            uncovered_resources = [k for k,v in uncovered.items() if v]
+            uncover_statement = f"SELECT {', '.join(uncovered_resources)}" + " FROM resources WHERE id=%s"
+            db.execute(uncover_statement, (eId,))
+            resources = db.fetchall()[0]
+
+            print(uncovered_resources)
+            print(resources)
+
+            # db.execute("INSERT INTO spyinfo (spyer, spyee, date) VALUES (%s, %s, %s)", (cId, eId, time.time()))
+
+            update_resources = []
+            for res, amo in zip(uncovered_resources, resources):
+                update_resources.append(f"{res}={amo}")
+            print(update_resources)
+            update_resources_string = ", ".join(update_resources)
+            print(update_resources_string)
+
+            spyinfo_update = f"UPDATE spyinfo SET {update_resources_string}" + " WHERE id=%s"
+
+            
+        # db.execute("UPDATE military SET spies=spies-%s WHERE id=%s", (executed_spies, cId))
+
+        connection.commit()
+        connection.close()
+
+        return redirect("/intelligence")
